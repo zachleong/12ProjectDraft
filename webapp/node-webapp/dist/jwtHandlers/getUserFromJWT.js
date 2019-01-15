ad72ab14b0dc70db696c41840468d6d1ad2f0f61
Zach Leong <earthospreyhd@gmail.com>
2019-01-15 12:22:28 +1100

    changed tsconfig and delted old files

--- webapp/node-webapp/dist/jwtHandlers/getUserFromJWT.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var user_1 = require("../dataStructures/user");
-var uuid = require("uuid");
-function getUserFromJWT(jwt) {
-    var thisUser = new user_1.User("default_user", uuid());
-    return thisUser;
-}
-exports.getUserFromJWT = getUserFromJWT;
